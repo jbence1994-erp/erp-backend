31951216d7ff757e4d461a457d58ddadab38a145
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-03 13:10:30 +0200

    Make Product and Supplier data models JPA entities

--- src/main/java/com/github/jbence1994/erp/inventory/model/Product.java
+++ src/main/java/com/github/jbence1994/erp/inventory/model/Product.java
@@ -1,16 +1,39 @@
 package com.github.jbence1994.erp.inventory.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String serialNumber;
+
     private BigDecimal price;
+
     private String unit;
+
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
     private Supplier supplier;
+
     private Integer onStock;
+
     private String photoFileName;
 
     public boolean hasPhoto() {
