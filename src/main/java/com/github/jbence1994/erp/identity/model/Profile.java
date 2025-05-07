4f88fe8cca68b36883b9af09acc23f2da6f9c258
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-07 14:15:55 +0200

    Add JoinColumn annotation

--- src/main/java/com/github/jbence1994/erp/identity/model/Profile.java
+++ src/main/java/com/github/jbence1994/erp/identity/model/Profile.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -32,6 +33,7 @@ public class Profile {
     private boolean isDeleted;
 
     @OneToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     public boolean hasPhoto() {
