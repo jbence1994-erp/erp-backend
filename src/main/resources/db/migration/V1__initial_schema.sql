9361d45a17a7c0980d440c3f4def30e85583319c
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-07 09:36:38 +0200

    Change database schema according to data models

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS erp
-    DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+    DEFAULT CHARACTER SET utf8
+    COLLATE utf8_hungarian_ci;
 
 USE erp;
 
@@ -24,6 +25,28 @@ CREATE TABLE IF NOT EXISTS products
     photo_file_name VARCHAR(41),
     CONSTRAINT fk_supplier
         FOREIGN KEY (supplier_id) REFERENCES suppliers (id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id         BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    email      VARCHAR(75)  NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS profiles
+(
+    id              BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username        VARCHAR(15)  NOT NULL,
+    password        VARCHAR(255) NOT NULL,
+    photo_file_name VARCHAR(41),
+    is_deleted      TINYINT      NOT NULL,
+    user_id         BIGINT       NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id) REFERENCES users (id)
             ON DELETE CASCADE
             ON UPDATE CASCADE
 );
