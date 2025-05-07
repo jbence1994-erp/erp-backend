079d1169ba58ecfe12d5eb862bc14e186b1bc4ec
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2025-05-07 10:20:35 +0200

    Define User and Profile data models for Identity module (#19)

---
+++ src/main/java/com/github/jbence1994/erp/identity/model/Profile.java
@@ -0,0 +1,39 @@
+package com.github.jbence1994.erp.identity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "profiles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String photoFileName;
+
+    private boolean isDeleted;
+
+    @OneToOne
+    private User user;
+
+    public boolean hasPhoto() {
+        return photoFileName != null;
+    }
+}
---
+++ src/main/java/com/github/jbence1994/erp/identity/model/User.java
@@ -0,0 +1,29 @@
+package com.github.jbence1994.erp.identity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}
