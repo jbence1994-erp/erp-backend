7022174bbedb9cb622252b22ddeb721e4e19b84e
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-07 09:34:44 +0200

    Define Profile model

---
+++ src/main/java/com/github/jbence1994/erp/identity/model/Profile.java
@@ -0,0 +1,39 @@
+package com.github.jbence1994.erp.identity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "profiles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String photoFileName;
+
+    private Boolean isDeleted;
+
+    @OneToOne(mappedBy = "profile")
+    private User user;
+
+    public Boolean hasPhoto() {
+        return photoFileName != null;
+    }
+}
