e437502f5d9527476d89ae01a01deabb9cc76c78
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2025-05-09 12:06:55 +0200

    Implement getting all products (#27)

--- src/test/java/com/github/jbence1994/erp/inventory/testobject/ProductTestObject.java
+++ src/test/java/com/github/jbence1994/erp/inventory/testobject/ProductTestObject.java
@@ -5,10 +5,14 @@ import com.github.jbence1994.erp.inventory.model.Product;
 import java.math.BigDecimal;
 
 import static com.github.jbence1994.erp.common.constant.PhotoTestConstants.PHOTO_FILE_NAME;
+import static com.github.jbence1994.erp.inventory.constant.ProductTestConstants.PRODUCT_1_DESCRIPTION;
 import static com.github.jbence1994.erp.inventory.constant.ProductTestConstants.PRODUCT_1_NAME;
-import static com.github.jbence1994.erp.inventory.constant.ProductTestConstants.PRODUCT_1_SERIAL_DESCRIPTION;
 import static com.github.jbence1994.erp.inventory.constant.ProductTestConstants.PRODUCT_1_SERIAL_NUMBER;
 import static com.github.jbence1994.erp.inventory.constant.ProductTestConstants.PRODUCT_1_UNIT;
+import static com.github.jbence1994.erp.inventory.constant.ProductTestConstants.PRODUCT_2_DESCRIPTION;
+import static com.github.jbence1994.erp.inventory.constant.ProductTestConstants.PRODUCT_2_NAME;
+import static com.github.jbence1994.erp.inventory.constant.ProductTestConstants.PRODUCT_2_SERIAL_NUMBER;
+import static com.github.jbence1994.erp.inventory.constant.ProductTestConstants.PRODUCT_2_UNIT;
 import static com.github.jbence1994.erp.inventory.testobject.SupplierTestObject.supplier1;
 
 public final class ProductTestObject {
@@ -19,7 +23,7 @@ public final class ProductTestObject {
                 PRODUCT_1_SERIAL_NUMBER,
                 BigDecimal.valueOf(10000.00),
                 PRODUCT_1_UNIT,
-                PRODUCT_1_SERIAL_DESCRIPTION,
+                PRODUCT_1_DESCRIPTION,
                 supplier1(),
                 10,
                 null
@@ -33,10 +37,24 @@ public final class ProductTestObject {
                 PRODUCT_1_SERIAL_NUMBER,
                 BigDecimal.valueOf(10000.00),
                 PRODUCT_1_UNIT,
-                PRODUCT_1_SERIAL_DESCRIPTION,
+                PRODUCT_1_DESCRIPTION,
                 supplier1(),
                 10,
                 PHOTO_FILE_NAME
+        );
+    }
+
+    public static Product product2() {
+        return new Product(
+                2L,
+                PRODUCT_2_NAME,
+                PRODUCT_2_SERIAL_NUMBER,
+                BigDecimal.valueOf(20000.00),
+                PRODUCT_2_UNIT,
+                PRODUCT_2_DESCRIPTION,
+                supplier1(),
+                20,
+                null
         );
     }
 }
