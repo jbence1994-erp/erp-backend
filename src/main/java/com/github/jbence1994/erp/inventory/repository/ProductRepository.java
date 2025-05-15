cacc31da6311b091e399d73e71d4b0561dd996e0
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-15 19:58:35 +0200

    Remove Repository annotations

--- src/main/java/com/github/jbence1994/erp/inventory/repository/ProductRepository.java
+++ src/main/java/com/github/jbence1994/erp/inventory/repository/ProductRepository.java
@@ -2,8 +2,6 @@ package com.github.jbence1994.erp.inventory.repository;
 
 import com.github.jbence1994.erp.inventory.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
